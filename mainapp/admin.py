74d26d29630a7e714a6fb258b7af250bffb74e79
temurhamidov <temur20012007@gmail.com>
2023-08-09 23:44:14 +0500

    Third commet

--- mainapp/admin.py
+++ mainapp/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Category, Product, Technicians, Contact
+from .models import Category, Product, Technicians, Contact, Service, ServiceBooking, ProductOrder
 
 
 @admin.register(Category)
@@ -9,6 +9,11 @@ class CategoryAdmin(admin.ModelAdmin):
 @admin.register(Contact)
 class ContactAdmin(admin.ModelAdmin):
     list_display = ['name', 'phone', 'created']
+
+
+@admin.register(Service)
+class ServiceAdmin(admin.ModelAdmin):
+    list_display = ['name', 'support_time']
 
 
 @admin.register(Technicians)
@@ -22,5 +27,18 @@ class ProductAdmin(admin.ModelAdmin):
     list_display = ['name', 'category', 'slug', 'created']
     list_filter = ['created']
     search_fields = ['name']
+
+@admin.register(ServiceBooking)
+class ServiceBookingAdmin(admin.ModelAdmin):
+    list_display = ['name', 'service', 'created']
+    list_filter = ['created']
+    search_fields = ['name']
+
+
+@admin.register(ProductOrder)
+class ProductBookingAdmin(admin.ModelAdmin):
+    list_display = ['name', 'phone', 'created']
+
+
 
 
--- mainapp/urls.py
+++ mainapp/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 from .views import HomeView, AboutView, ContactView, TeamView, ProductView, ProductListWithCategory, ServiceView, BookingView, AllProductsView, TestimonialView
-from .views import CartView
+from .views import CartView, ProductOrder
 app_name = 'mainapp'
 urlpatterns = [
     # Home
@@ -14,6 +14,7 @@ urlpatterns = [
     path('product/', ProductView.as_view(), name='product'),
     path('all-products/', AllProductsView.as_view(), name='all-products'),
     path('cart/', CartView.as_view(), name='cart'),
+    path('product-order/', ProductOrder.as_view(), name='product-order'),
     path('products/<slug:slug>', ProductListWithCategory.as_view(), name='products_category'),
 
 ]
